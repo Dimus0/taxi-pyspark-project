6e1a4e02682790c230145d009aeacf1b927f9396
Dimus0 <dimapugachevskiy08@gmail.com>
2025-11-03 19:17:02 +0200

    error correction

--- functions/extraction.py
+++ functions/extraction.py
@@ -1,69 +1,49 @@
+from os import path
 from pyspark.sql.types import DateType,StructType, StructField,StringType, LongType, DoubleType,IntegerType, TimestampType
-
+from pyspark.sql.functions import col
+from pyspark.sql import SparkSession
+from pathlib import Path
 """
     Етап видобування
 """
-
-# Створенно схему набору даних
 trip_schema = StructType([
-    StructField("hvfhs_license_num", StringType(), True),       # ID компанії-оператора
-    StructField("dispatching_base_num", StringType(), True),    # база, яка надіслала замовлення
-    StructField("originating_base_num", StringType(), True),    # база походження
+    StructField("hvfhs_license_num", StringType(), True),
+    StructField("dispatching_base_num", StringType(), True),
+    StructField("originating_base_num", StringType(), True),
     StructField("request_datetime", TimestampType(), True),
-    StructField("on_scene_datetime", TimestampType(), True),
     StructField("pickup_datetime", TimestampType(), True),
     StructField("dropoff_datetime", TimestampType(), True),
-    StructField("PULocationID", LongType(), True),
-    StructField("DOLocationID", LongType(), True),
+    StructField("PULocationID", IntegerType(), True),
+    StructField("DOLocationID", IntegerType(), True),
     StructField("trip_miles", DoubleType(), True),
-    StructField("trip_time", LongType(), True),
+    StructField("trip_time", IntegerType(), True),
     StructField("base_passenger_fare", DoubleType(), True),
     StructField("tolls", DoubleType(), True),
     StructField("bcf", DoubleType(), True),
     StructField("sales_tax", DoubleType(), True),
     StructField("congestion_surcharge", DoubleType(), True),
-    StructField("airport_fee", IntegerType(), True),
+    StructField("airport_fee", DoubleType(), True),
     StructField("tips", DoubleType(), True),
-    StructField("driver_pay", DoubleType(), True),
-    StructField("shared_request_flag", StringType(), True),
-    StructField("shared_match_flag", StringType(), True),
-    StructField("access_a_ride_flag", StringType(), True),
-    StructField("wav_request_flag", StringType(), True),
-    StructField("wav_match_flag", IntegerType(), True)
+    StructField("driver_pay", DoubleType(), True)
 ])
 
-bases_schema = StructType([
-    StructField("base_num", StringType(), False),   # Primary key
-    StructField("base_name", StringType(), True),
-    StructField("base_city", StringType(), True),
-    StructField("fleet_size", IntegerType(), True)
+# Додаткові таблиці
+base_schema = StructType([
+    StructField("base_num", StringType(), True)
 ])
 
-locations_schema = StructType([
-    StructField("LocationID", LongType(), False),  # Primary key
-    StructField("Borough", StringType(), True),
-    StructField("Zone", StringType(), True),
-    StructField("service_zone", StringType(), True)
+vehicle_schema = StructType([
+    StructField("hvfhs_license_num", StringType(), True)
 ])
 
-drivers_schema = StructType([
-    StructField("driver_id", StringType(), False),   # Primary key
-    StructField("hvfhs_license_num", StringType(), True),
-    StructField("driver_name", StringType(), True),
-    StructField("experience_years", IntegerType(), True),
-    StructField("rating", DoubleType(), True)
+zone_schema = StructType([
+    StructField("zone_id", IntegerType(), True)
 ])
 
-calendar_schema = StructType([
-    StructField("datetime", TimestampType(), False),
-    StructField("date", DateType(), True),
-    StructField("hour", IntegerType(), True),
-    StructField("day_of_week", StringType(), True),
-    StructField("month", IntegerType(), True),
-    StructField("year", IntegerType(), True)
-])
+def run_extracion(spark: SparkSession,input_path: str):
+    
+    path = Path(input_path)
+    if not path.exists():
+        raise FileNotFoundError(f"File not found {input_path}")
 
-def run_extracion():
-
-
-    pass # <---- 
+    pass
