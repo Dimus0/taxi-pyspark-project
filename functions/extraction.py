f176f99e6139d68bef571e59e30cc6f271ad8ae8
Dimus0 <dimapugachevskiy08@gmail.com>
2025-11-07 15:55:06 +0200

    створенні 6 бізнес питаннь by Dmytro

--- functions/extraction.py
+++ functions/extraction.py
@@ -1,13 +1,55 @@
 from os import path
 from pyspark.sql.types import DateType, StructType, StructField, StringType, LongType, DoubleType, IntegerType, \
     TimestampType,BinaryType
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col,create_map,lit,when
 from pyspark.sql import SparkSession
 from pathlib import Path
 
 """
     Етап видобування
 """
+
+list_name_base = {
+    "B02914": "VULCAN CARS LLC",
+    "B02907": "SABO ONE LLC",
+    "B02908": "SABO TWO LLC",
+    "B03035": "OMAHA LLC",
+    "B02510": "TRI-CITY, LLC",
+    "B02844": "ENDOR CAR & DRIVER, LLC",
+    "B02877": "ZWOLF-NY, LLC",
+    "B02866": "ZWEI-NY, LLC",
+    "B02882": "ZWANZIG-NY, LLC",
+    "B02869": "ZEHN-NY, LLC",
+    "B02617": "WEITER LLC",
+    "B02876": "VIERZEHN-NY, LLC",
+    "B02865": "VIER-NY, LLC",
+    "B02512": "UNTER LLC",
+    "B02888": "SIEBZEHN-NY, LLC",
+    "B02864": "SIEBEN-NY, LLC",
+    "B02883": "SECHZEHN-NY, LLC",
+    "B02875": "SECHS-NY, LLC",
+    "B02682": "SCHMECKEN LLC",
+    "B02880": "NEUNZEHN-NY, LLC",
+    "B02870": "NEUN-NY, LLC",
+    "B02404": "KUCHEN, LLC",
+    "B02598": "HINTER LLC",
+    "B02765": "GRUN LLC",
+    "B02879": "FUNFZEHN-NY, LLC",
+    "B02867": "FUNF-NY, LLC",
+    "B02878": "ELF-NY, LLC",
+    "B02887": "EINUNDZWANZIG-NY, LLC",
+    "B02872": "EINS-NY, LLC",
+    "B02836": "DRINNEN-NY LLC",
+    "B02884": "DREIZEHN-NY, LLC",
+    "B02835": "DREIST NY LLC",
+    "B02764": "DANACH-NY, LLC",
+    "B02889": "ACHTZEHN-NY, LLC",
+    "B02871": "ACHT-NY, LLC",
+    "B02395": "ABATAR LLC",
+    "B03136": "GREENPOINT TRANSIT LLC ",
+    "B02800": "FLATIRON TRANSIT LLC "
+}
+
 trip_schema = StructType([
     StructField("hvfhs_license_num", StringType(), True),
     StructField("dispatching_base_num", StringType(), True),
@@ -79,6 +121,7 @@ def run_extracion(spark: SparkSession, input_path: str):
     # Зчитування csv файлу.
     df_trip = spark.read.schema(trip_schema).parquet(*[str(f) for f in parquet_files])
 
+
     # Обмеження даних з кожного parquet file по 10к з кожнго це буде 460к
     limit_per_file = 10000
 
@@ -93,19 +136,25 @@ def run_extracion(spark: SparkSession, input_path: str):
     for df in limited[1:]:
         df_trip = df_trip.unionByName(df, allowMissingColumns=True)
 
-    print(f"Після об’єднання рядків: {df_trip.count()}, колонок: {len(df_trip.columns)}")
+
+    # print(f"Після об’єднання рядків: {df_trip.count()}, колонок: {len(df_trip.columns)}")
+    df_trip = replacment_names_in_columns(df_trip)
 
     df_dispatch_base = df_trip.selectExpr("dispatching_base_num as dispatch_base_num").distinct()
     df_origin_base = df_trip.selectExpr("originating_base_num as origin_base_num").distinct()
 
     df_vehicle = df_trip.select("hvfhs_license_num").distinct()
 
-    print("DataFrames успішно створені:")
-    print(f" - df_trip: {df_trip.count()} рядків")
-    print(f" - df_dispatch_base: {df_dispatch_base.count()} унікальних dispatching_base_num")
-    print(f" - df_origin_base: {df_origin_base.count()} унікальних originating_base_num")
-    print(f" - df_vehicle: {df_vehicle.count()} унікальних hvfhs_license_num")
-    print(f" - df_locations: {df_location_dim.count()} rows")
+
+    '''
+        Закоментовано для швидкого переходу до іншого етапу, у разі потреба розкометувати для відображення інофрмації
+    '''
+    # print("DataFrames успішно створені:")
+    # print(f" - df_trip: {df_trip.count()} рядків")
+    # print(f" - df_dispatch_base: {df_dispatch_base.count()} унікальних dispatching_base_num")
+    # print(f" - df_origin_base: {df_origin_base.count()} унікальних originating_base_num")
+    # print(f" - df_vehicle: {df_vehicle.count()} унікальних hvfhs_license_num")
+    # print(f" - df_locations: {df_location_dim.count()} rows")
 
     # Перевірка DataFrame
     # dataframe_verification(df_trip,df_dispatch_base,df_origin_base,df_vehicle,df_location)
@@ -154,3 +203,26 @@ def dataframe_verification(df_trip,df_dispatch_base,df_origin_base,df_vehicle,df
     df_origin_base.printSchema()
     df_vehicle.printSchema()
     df_location.printSchema()
+
+def replacment_names_in_columns(df_trip):
+    mapping_expr = create_map([lit(x) for pair in list_name_base.items() for x in pair])
+
+    df_trip = (
+        df_trip
+        .withColumn("dispatching_base_num",mapping_expr[col("dispatching_base_num")])
+        .withColumn("originating_base_num",mapping_expr[col("originating_base_num")])
+    )
+
+    df_trip = (
+        df_trip
+        .withColumn(
+            "hvfhs_license_num",
+            when(col("hvfhs_license_num") == "HV0002", "Juno")
+            .when(col("hvfhs_license_num") == "HV0003", "Uber")
+            .when(col("hvfhs_license_num") == "HV0004", "Via")
+            .when(col("hvfhs_license_num") == "HV0005", "Lyft")
+            .otherwise(col("hvfhs_license_num"))
+        )
+    )
+
+    return df_trip
